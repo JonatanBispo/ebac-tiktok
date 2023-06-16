20a5d333aabe534c13f2181a477d4fa958ffc147
Jonatan Bispo <jonatansantos1992@gmail.com>
2023-06-16 14:08:29 -0300

    refatorado

--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./videoFooter.css";
-import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
 
 function VideoFooter() {
   return (
@@ -9,16 +9,16 @@ function VideoFooter() {
         <h3>@Jonatan Bispo</h3>
         <p>Descrição do video</p>
         <div className="videoFooter__music">
-          <MusicNoteIcon className="videoFooter__icon"/>
+          <MusicNoteIcon className="videoFooter__icon" />
           <div className="videoFooterMusic__text">
-          <p>Titulo da musica</p>
+            <p>Titulo da musica</p>
           </div>
         </div>
       </div>
-      <img 
-      className="videoFooter__record"
-      alt="Imagem de um vinil girando"
-      src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png?t=2023-05-22T19%3A39%3A28.772Z"
+      <img
+        className="videoFooter__record"
+        alt="Imagem de um vinil girando"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png?t=2023-05-22T19%3A39%3A28.772Z"
       />
     </div>
   );
